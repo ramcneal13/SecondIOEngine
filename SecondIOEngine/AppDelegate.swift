5544d0969cfe022de86f5bf53cf7c199d8c0fcb9
Richard McNeal <rick.mcneal@hgst.com>
2017-11-04 12:48:08 -0700

    Initial Commit

---
+++ SecondIOEngine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SecondIOEngine
+//
+//  Created by Richard McNeal on 11/4/17.
+//  Copyright © 2017 Richard McNeal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SecondIOEngine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SecondIOEngine
+//
+//  Created by Richard McNeal on 11/4/17.
+//  Copyright © 2017 Richard McNeal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
